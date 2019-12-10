961a91b9b42bea64b78958a9faf2e874969049ce
Darian Jarvis <djarvis@nyc.yearup.org>
2019-12-10 14:35:41 -0500

    added hover animation to make wave feel.

--- server.rb
+++ server.rb
@@ -1,8 +1,37 @@
 require 'sinatra'
-require 'chucknorris'
+require 'chuck_norris'
+
+# get '/' do
+#
+#   erb :index
+#    # ChuckNorris.random
+# end
 
 get '/' do
+# chucknorris joke
+@random_joke = ChuckNorris::JokeFinder.get_joke
+erb :index
+end
 
-  erb :index
-   # ChuckNorris.random
+get '/joke/:id' do
+  joke_id = params[':id'].to_i
+  @found_joke = ChuckNorris::JokeFinder.find_joke(joke_id)
+  @found_joke.joke
+end
+
+get '/categories' do
+  # call the category listing method
+  @categories = ChuckNorris::JokeFinder.joke_categories
+  # puts ChuckNorris::JokeFinder
+# puts @categories
+# puts @categories.methods
+  # return an ERB file with a list of categories that also link to a page of those categories
+  erb :categories
+
+end
+
+get '/category/:name' do
+  @category_name = params['name']
+  @catergory_name
+
 end
